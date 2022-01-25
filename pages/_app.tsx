819f97b8167f4b5abdb0fcccd74b442c65945e49
UsamaInsight <usamainsight@gmail.com>
2022-01-25 11:42:05 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_app.tsx
@@ -0,0 +1,84 @@
+import "../styles.css";
+import dynamic from "next/dynamic";
+import { TinaEditProvider } from "tinacms/dist/edit-state";
+import { Layout } from "../components/layout";
+// @ts-ignore FIXME: default export needs to be 'ComponentType<{}>
+const TinaCMS = dynamic(() => import("tinacms"), { ssr: false });
+
+const branch = "main";
+const apiURL =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:4001/graphql"
+    : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <TinaEditProvider
+        showEditButton={true}
+        editMode={
+          <TinaCMS
+            apiURL={apiURL}
+            mediaStore={async () => {
+              const pack = await import("next-tinacms-cloudinary");
+              return pack.TinaCloudCloudinaryMediaStore;
+            }}
+            cmsCallback={(cms) => {
+              /**
+               * Enables experimental branch switcher
+               */
+              cms.flags.set("branch-switcher", true);
+
+              /**
+               * Enables `tina-admin` specific features in the Tina Sidebar
+               */
+              cms.flags.set("tina-admin", false);
+            }}
+            documentCreatorCallback={{
+              /**
+               * After a new document is created, redirect to its location
+               */
+              onNewDocument: ({ collection: { slug }, breadcrumbs }) => {
+                const relativeUrl = `/${slug}/${breadcrumbs.join("/")}`;
+                return (window.location.href = relativeUrl);
+              },
+              /**
+               * Only allows documents to be created to the `Blog Posts` Collection
+               */
+              filterCollections: (options) => {
+                return options.filter(
+                  (option) => option.label === "Blog Posts"
+                );
+              },
+            }}
+            formifyCallback={({ formConfig, createForm, createGlobalForm }) => {
+              if (formConfig.id === "getGlobalDocument") {
+                return createGlobalForm(formConfig);
+              }
+
+              return createForm(formConfig);
+            }}
+            {...pageProps}
+          >
+            {(livePageProps) => (
+              <Layout
+                rawData={livePageProps}
+                data={livePageProps.data?.getGlobalDocument?.data}
+              >
+                <Component {...livePageProps} />
+              </Layout>
+            )}
+          </TinaCMS>
+        }
+      >
+        <Layout
+          rawData={pageProps}
+          data={pageProps.data?.getGlobalDocument?.data}
+        >
+          <Component {...pageProps} />
+        </Layout>
+      </TinaEditProvider>
+    </>
+  );
+};
+
+export default App;
